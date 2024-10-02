b513ba848cf26d43926b348d7a5c11a31dfbfd1b
Arpandev-code <arpan.dev2016@gmail.com>
2024-10-02 13:39:00 +0530

    minor fix in search field

--- lib/views/widgets/searchfield.dart
+++ lib/views/widgets/searchfield.dart
@@ -10,19 +10,19 @@ class Searchfield extends StatelessWidget {
         hintText: 'Search product...',
         hintStyle: TextStyle(color: Colors.grey[600]),
         prefixIcon: Icon(Icons.search, color: Colors.grey[600]),
-        suffixIcon: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            IconButton(
-              icon: Icon(Icons.mic, color: Colors.grey[600]),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.camera_alt_outlined, color: Colors.grey[600]),
-              onPressed: () {},
-            ),
-          ],
-        ),
+        // suffixIcon: Row(
+        //   mainAxisSize: MainAxisSize.min,
+        //   children: [
+        //     IconButton(
+        //       icon: Icon(Icons.mic, color: Colors.grey[600]),
+        //       onPressed: () {},
+        //     ),
+        //     IconButton(
+        //       icon: Icon(Icons.camera_alt_outlined, color: Colors.grey[600]),
+        //       onPressed: () {},
+        //     ),
+        //   ],
+        // ),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10.0),
           borderSide: const BorderSide(color: Colors.purple),
